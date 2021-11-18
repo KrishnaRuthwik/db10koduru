870559ec7bc0c2d3499d52ab83caa85d359d8671
KrishnaRuthwik <89400910+KrishnaRuthwik@users.noreply.github.com>
2021-11-17 19:44:49 -0600

    added files

--- routes/costume.js
+++ routes/costume.js
@@ -6,4 +6,8 @@ var router = express.Router();
 router.get('/', costume_controlers.costume_view_all_Page ); 
 /* GET detail costume page */ 
 router.get('/detail', costume_controlers.costume_view_one_Page); 
+/* GET create costume page */ 
+router.get('/create', costume_controlers.costume_create_Page); 
+/* GET create update page */ 
+router.get('/update', costume_controlers.costume_update_Page); 
 module.exports = router; 
